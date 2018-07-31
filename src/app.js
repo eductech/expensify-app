4de4c91939c98babf3bf29dc5dd6351154b0019d
Viktor Zamirouski <studiaformanta@gmail.com>
2018-07-31 22:59:04 +0300

    lesson 78

--- src/app.js
+++ src/app.js
@@ -1,10 +1,44 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
-import IndecisionApp from './components/IndecisionApp';
+import { BrowserRouter, Route } from 'react-router-dom';
 
 //styles
 import 'normalize.css/normalize.css';
-import './styles/styles.scss';
+import './styles/styles.scss'; 
 
-ReactDOM.render(<IndecisionApp />, document.getElementById('app'));
+const ExpenseDashboardPage = () => (
+  <div>
+    this is my react component
+  </div>
+);
+
+const AddExpensePage = () => (
+  <div>
+    this is my expense page
+  </div>
+);
+
+const EditExpensePage = () => (
+  <div>
+    this is my edit
+  </div>
+);
+
+const HelpPage = () => (
+  <div>
+    this is my help page
+  </div>
+);
+
+const routes = (
+  <BrowserRouter>
+    <div>
+      <Route path="/" component={ExpenseDashboardPage} exact={true} />
+      <Route path="/create" component={AddExpensePage} />
+      <Route path="/edit" component={EditExpensePage} />
+      <Route path="/help" component={HelpPage} />
+    </div>
+  </BrowserRouter>
+);
+
+ReactDOM.render(routes, document.getElementById('app'));
--- src/playground/es6-arrow-functions.js
+++
@@ -1,4 +0,0 @@
-let name = "Pit Doe";
-const getFirstName = (name) => name.split(' ')[0];
-
-console.log(getFirstName(name));
