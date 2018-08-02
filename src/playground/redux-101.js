b05d1d79475b14dcc593ba52c6c4b09f82e831a7
Aliaksander Zamirouski <studiaformanta@yandex.ru>
2018-08-02 23:56:55 +0300

    lesson 93

--- src/playground/redux-101.js
+++ src/playground/redux-101.js
@@ -20,7 +20,7 @@ const resetCount = () => ({
   number: 0
 });
 
-const store = createStore((state = {count: 0}, action) => {
+const countReducer = (state = {count: 0}, action) => {
   switch (action.type) {
     case 'INCREMENT':
       return {
@@ -41,7 +41,9 @@ const store = createStore((state = {count: 0}, action) => {
     default: 
       return state;
   }
-});
+}
+
+const store = createStore(countReducer);
 
 const unsubscribe = store.subscribe(() => {
   console.log(store.getState());
