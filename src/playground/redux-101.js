0b6e27929c1a94e8e4183bba500ea0302a615b16
Aliaksader Zamirovski <studiaformanta@yandex.ru>
2018-08-02 16:33:00 +0300

    lesson 91

--- src/playground/redux-101.js
+++ src/playground/redux-101.js
@@ -1,34 +1,66 @@
 import { createStore } from 'redux';
 
+const incrementCount = ({incrementBy = 1} = {}) => ({
+  type: 'INCREMENT',
+  incrementBy
+});
+
+const decrementCount = ({decrementBy = 1} = {}) => ({
+  type: 'DECREMENT',
+  decrementBy
+});
+
+const setCount = ({ count }) => ({
+  type: 'SET',
+  count
+});
+
+const resetCount = () => ({
+  type: 'RESET',
+  number: 0
+});
+
 const store = createStore((state = {count: 0}, action) => {
   switch (action.type) {
     case 'INCREMENT':
       return {
-        count: state.count + 1
+        count: state.count + action.incrementBy
       };
     case 'DECREMENT':
       return {
-        count: state.count - 1
+        count: state.count - action.decrementBy
       };
     case 'RESET':
       return {
         count: 0
+      };
+    case 'SET':
+      return {
+        count: action.count 
       };
     default: 
       return state;
   }
 });
 
-store.dispatch({
-  type: "INCREMENT"
+const unsubscribe = store.subscribe(() => {
+  console.log(store.getState());
 });
 
 store.dispatch({
-  type: "RESET"
+  type: "INCREMENT",
+  incrementBy: 5
 });
+
+store.dispatch(incrementCount());
+
+store.dispatch(decrementCount({ decrementBy: 50 }));
+
+store.dispatch(resetCount());
+
+store.dispatch(setCount({ count: 50 }));
 
 store.dispatch({
-  type: "DECREMENT"
+  type: "DECREMENT",
+  decrementBy: 100
 });
-
-console.log(store.getState());
