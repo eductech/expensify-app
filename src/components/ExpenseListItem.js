229b76dea3516dc3badba11a54811882c8cbb5fb
Aliaksader Zamirovski <studiaformanta@yandex.ru>
2018-08-09 14:48:01 +0300

    lesson 138

--- src/components/ExpenseListItem.js
+++ src/components/ExpenseListItem.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import moment from "moment";
 
 const ExpenseListItem = ({id, description, createdAt, amount}) => {
   return (
     <div>
       <Link to={`/edit/${id}`}>Edit</Link>
       <h3>{description}</h3>
-      <p>{amount} - {createdAt}</p>
+      <p>
+        {amount} - {moment(createdAt).format('MMMM Do, YYYY')}
+      </p>
     </div>
   );
 };
