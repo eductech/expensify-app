fa8580fbbc01ca8f7208e6b148908b0c3aa6a838
Aliaksader Zamirovski <studiaformanta@yandex.ru>
2018-08-16 16:43:38 +0300

    lesson 156

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -1,12 +1,12 @@
 import * as firebase from 'firebase';
 
 const config = {
-  apiKey: "AIzaSyAmyn4sgiSvdAtI31H4tQrI4CznQJF1ddY",
-  authDomain: "expensify-d2d8d.firebaseapp.com",
-  databaseURL: "https://expensify-d2d8d.firebaseio.com",
-  projectId: "expensify-d2d8d",
-  storageBucket: "expensify-d2d8d.appspot.com",
-  messagingSenderId: "270862135862"
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.FIREBASE_DATABASE_URL,
+  projectId: process.env.PROJECT_ID,
+  storageBucket: process.env.STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID
 };
 
 firebase.initializeApp(config);
